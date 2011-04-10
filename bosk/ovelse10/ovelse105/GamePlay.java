53e94c83ad473073cf853cd4caf25382719820fe
Jakob Melnyk <jakobmelnyk@hotmail.com>
2011-04-10 20:20:46 +0200

    lavede den faerdig

--- bosk/ovelse10/ovelse105/GamePlay.java
+++ bosk/ovelse10/ovelse105/GamePlay.java
@@ -10,13 +10,13 @@ public class GamePlay {
 
 	// Lister der holder styr på historik - se metoden update() i GameUtils
 
-	//	 listen fightparticipants gemmer historik over hvilke spillere der har kæmpet mod hinanden
+	// Listen fightparticipants gemmer historik over hvilke spillere der har kæmpet mod hinanden
 	static List<Fight<Player>> fightparticipants = new ArrayList<Fight<Player>>();
 
-	//	 listen fighttypes gemmer historik over hvilke typer af spillere (Thief, Wizard, etc) der har kæmpet mod hinanden.
+	//L isten fighttypes gemmer historik over hvilke typer af spillere (Thief, Wizard, etc) der har kæmpet mod hinanden.
 	static List<Fight<String>>  fighttypes = new ArrayList<Fight<String>>();
 
-	//	 listen smartfight gemmer historik over hvordan det går to kæmpende med hver deres intelligensniveau. Kan bruges til at
+	// Listen smartfight gemmer historik over hvordan det går to kæmpende med hver deres intelligensniveau. Kan bruges til at
 	// regulere i spillet så vi ikke får for mange stupide vindere :-)
 	static List<Fight<Smartness>> smartfight = new ArrayList<Fight<Smartness>>();
 
@@ -24,7 +24,7 @@ public class GamePlay {
 	static List<Fight<Strength>>  strongfight = new ArrayList<Fight<Strength>>();
 
 	// Figurerne i spillet
-	static List players = new ArrayList();
+	static List<Player> players = new ArrayList<Player>();
 
 	public static void main(String[] args) {
 		// Vi laver nogle figurer og giver dem nogle egenskaber (massiv brug af enums)
@@ -63,7 +63,7 @@ public class GamePlay {
 		// to argumenter med typen Warrior giver Warrior som returtype
 		// Sørg for at vi ikke behøver at caste
 		System.out.println("Knud and Svend fights dirty:");
-		Warrior w1  = (Warrior) GameUtil.fightDirty(knud, svend);
+		Warrior w1  = GameUtil.fightDirty(knud, svend);
 		System.out.println("The winner says: " + w1.speak());
 
 		System.out.println();
@@ -71,7 +71,7 @@ public class GamePlay {
 		// to argumenter med typen Wizard giver Wizard som returtype
 		// Sørg for at vi ikke behøver at caste
 		System.out.println("Merlin and Andrew fights dirty:");
-		Wizard w2 = (Wizard) GameUtil.fightDirty(merlin, andrew);
+		Wizard w2 = GameUtil.fightDirty(merlin, andrew);
 		System.out.println("The winner says: " + w2.speak());
 
 		System.out.println();
@@ -87,23 +87,23 @@ public class GamePlay {
 		// to slags Magician kan blive til Player eller Magician
 		// Sørg for at vi ikke behøver at caste til en Magician
 		System.out.println("Jadis and Merlin fight magical:");
-		Magician m = (Magician) GameUtil.fightMagical(jadis, merlin);
+		Magician m = GameUtil.fightMagical(jadis, merlin);
 		Player mp = GameUtil.fightMagical(jadis, merlin);
 		// men vi kan ikke vide om det er en Witch eller en Wizard der vinder
 		// så følgende må ikke være tilladt:
 		// Witch wit = GameUtil.fightMagical(jadis, merlin);
 		// Wizard wiz = GameUtil.fightMagical(jadis, merlin);
 
-		//		 bemærk at fightMagical ikke bør kuhne kaldes med Mortals
+		//bemærk at fightMagical ikke bør kuhne kaldes med Mortals
 		// Sørg for at nedenstående kald ikke er lovligt
-		Player p2 = GameUtil.fightMagical(x, svend);
+		//Player p2 = GameUtil.fightMagical(x, svend);
 		System.out.println("The winner says: " + mp.speak());
 
 		System.out.println();
 
 		// to slags Mortals kan blive til en Mortal eller en Player
 		System.out.println("Y and Svend fight dirty:");
-		Mortal mort = (Mortal) GameUtil.fightDirty(y, svend);
+		Mortal mort = GameUtil.fightDirty(y, svend);
 		Player play = GameUtil.fightDirty(y, svend);
 		// men ikke en Magician eller en Thief eller en Warrior
 		// Magician mag = GameUtil.fightDirty(y, svend);
--- bosk/ovelse10/ovelse105/GameUtil.java
+++ bosk/ovelse10/ovelse105/GameUtil.java
@@ -14,7 +14,7 @@ public class GameUtil {
 		return findWinner(one, two, i);
 	}
 
-	public static <N extends Player> N fightMagical(N one, N two){
+	public static <N extends Magician> N fightMagical(N one, N two){
 		Magician m1 = (Magician) one;
 		Magician m2 = (Magician) two;
 		int i = m1.getMagicPowers().compareTo(m2.getMagicPowers());
@@ -47,15 +47,15 @@ public class GameUtil {
 		// Vi kan lave alle mulige slags Fight-instanser:
 
 		// listen fightparticipants gemmer historik over hvilke spillere der har kæmpet mod hinanden
-		GamePlay.fightparticipants.add(new Fight(one, two, winner));
+		GamePlay.fightparticipants.add(new Fight<Player>(one, two, winner));
 
 		// listen fighttypes gemmer historik over hvilke typer af spillere (Thief, Wizard, etc) der har kæmpet mod hinanden. Bemærk
 		// metoden getClass().getSimpleName() giver os navnet på den klasse som objektet er instantieret af
-		GamePlay.fighttypes.add(new Fight(one.getClass().getSimpleName(), two.getClass().getSimpleName(), winner.getClass().getSimpleName()));
+		GamePlay.fighttypes.add(new Fight<String>(one.getClass().getSimpleName(), two.getClass().getSimpleName(), winner.getClass().getSimpleName()));
 
 		// listen smartfight gemmer historik over hvordan det går to kæmpende med hver deres intelligensniveau. Kan bruges til at
 		// regulere i spillet så vi ikke får for mange stupide vindere :-)
-		GamePlay.smartfight.add(new Fight(one.smartness, two.smartness, winner.smartness));
+		GamePlay.smartfight.add(new Fight<Smartness>(one.smartness, two.smartness, winner.smartness));
 
 		// Hvis Fight parameteriseres kan man ikke indsætte forkert
 		// GamePlay.smartfight.add(new Fight<Smartness>(one.strength, two.speak(), winner.getClass())); er ikke lovligt
